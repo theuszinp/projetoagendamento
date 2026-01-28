4de067150f7dab19865de6878181e87eb4a33a0a
Matheus <154545786+theuszinp@users.noreply.github.com>
2026-01-28 12:43:10 -0300

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\develop\Flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Matheus\Downloads\projetoagendamento_revisado_v7\projetoagendamento_fix_autopostman2.0\rastreador_app_flutter"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
